9dda91e2cb84ccdecaaf0073d3e84957a57600ba
Lukas Winkler <git@lw1.at>
2021-11-12 17:13:35 +0100

    embed recordings

--- pretalx_matomocamp/signals.py
+++ pretalx_matomocamp/signals.py
@@ -16,13 +16,42 @@ class MatomoVideoProvider(BaseRecordingProvider):
         button_title = "View Workshop" if is_workshop else "View Livestream"
         livestream_url = f"https://live.matomocamp.org/" + submission.code
         chat_url = livestream_url + "/chat_room"
+        recording_url = livestream_url + "/recording"
+        recording_embed_url = livestream_url + "/recording_embed"
         return {
             "iframe": f"""
-            <div>
-            <a href='{livestream_url}' class='btn btn-primary'>{button_title}</a>
-            <a href="{chat_url}" class="btn btn-primary">Join Chatroom</a>
-            </div>
-            """,
+<style>
+.ratio {{ margin-top:1rem}}
+/* based on https://github.com/twbs/bootstrap/blob/f61a0218b36d915db80dc23635a9078e98e2e3e0/scss/helpers/_ratio.scss */
+.ratio {{
+    position: relative;
+    width: 100%;
+}}
+.ratio::before {{
+    display: block;
+    padding-top: calc(9 / 16 * 100%);
+    content: "";
+}}
+
+.ratio > * {{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}}
+</style>
+<div>
+    <a href="{recording_url}" class="btn btn-primary">View Recording</a>
+    <a href="{chat_url}" class="btn btn-primary">Join Chatroom</a>
+</div>
+<div class="ratio ratio-16x9" id="peertube-iframe">
+    <iframe src="{recording_embed_url}"
+            allowfullscreen
+            sandbox="allow-same-origin allow-scripts allow-popups"
+    ></iframe>    
+</div>""",
+            "csp_header": "https://video.matomocamp.org/ https://live.matomocamp.org/"
         }
 
 
